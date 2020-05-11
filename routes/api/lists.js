b03463b0755aa29ffa720e29e0d95f25f71995c0
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-11 15:59:43 -0600

    Implemented RESTful Get List by ID Router in API layer

--- routes/api/lists.js
+++ routes/api/lists.js
@@ -18,4 +18,17 @@ router.get('/', async function (req, res) {
     }
 });
 
+router.get('/:id', async function (req, res) {
+    try{
+        const list = await getListByID(req.params.id);
+        res.send(list);
+    }catch(err) {
+        if(err.error){
+            res.status(400).send(err);
+        }
+        console.log(err);
+        res.status(500).send('Internal server issue, check logs');
+    }
+});
+
 module.exports = router;
