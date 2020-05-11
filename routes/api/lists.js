a5d858e13c1496f36e93230d87f6386dede6fdc3
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-11 16:15:59 -0600

    Implemented RESTful add list Router in API

--- routes/api/lists.js
+++ routes/api/lists.js
@@ -23,11 +23,26 @@ router.get('/:id', async function (req, res) {
         const list = await getListByID(req.params.id);
         res.send(list);
     }catch(err) {
-        if(err.error){
+        if(err.error) {
             res.status(400).send(err);
         }
         console.log(err);
         res.status(500).send('Internal server issue, check logs');
+    }
+});
+
+//Post Router
+router.post('/', async function (req, res) {
+    try{
+        const newList = await addList(req.body);
+        res.send(newList);
+    }catch(err) {
+        if(err.error) {
+            res.status(400).send(err);
+        }else{
+            console.log(err);
+            res.status(500).send('Internal server issue, check logs');
+        }
     }
 });
 
