30c088533e70b7cb693d414abc099c85492fcd5b
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-11 15:49:23 -0600

    Implemented RESTful Get Lists Router in API layer

--- routes/api/lists.js
+++ routes/api/lists.js
@@ -1,4 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const {
+    getLists,
+    getListByID,
+    addList,
+    updateListValues
+} = require('./../../dal/lists');
+
+//Get Routers
+router.get('/', async function (req, res) {
+    try{
+        const lists = await getLists();
+        res.send(lists);
+    }catch(err) {
+        console.log(err);
+        res.status(500).send('Internal server issue, check logs');
+    }
+});
 
 module.exports = router;
