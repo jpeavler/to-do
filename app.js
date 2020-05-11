dd5c14c499d5910992fdccbb12e24eb96504b40d
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-11 15:52:02 -0600

    Utilized listsRouter

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/lists', listsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
